49b6ab7f2dbb2b735d957f7849193068dddefe9d
Галкин Сергей <galkinsa@edu.mos.ru>
2020-08-31 02:21:21 +0300

    Правки

--- go.mod
+++ go.mod
@@ -4,8 +4,6 @@ go 1.14
 
 require (
 	github.com/beevik/ntp v0.3.0
-	github.com/go-loremipsum/loremipsum v1.1.0
 	github.com/stretchr/testify v1.6.1 // indirect
-	golang.org/x/lint v0.0.0-20200302205851-738671d3881b // indirect
-	golang.org/x/tools v0.0.0-20200828161849-5deb26317202 // indirect
+	golang.org/x/net v0.0.0-20200625001655-4c5254603344 // indirect
 )
--- main.go
+++ main.go
@@ -3,16 +3,13 @@ package main
 import (
 	"fmt"
 	"github.com/beevik/ntp"
-	"os"
+	"log"
 )
 
 func main() {
 	time, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
 	if err != nil {
-		if _, err := fmt.Fprintln(os.Stderr, "Ошибка в библиотеке"); err != nil {
-			os.Exit(0)
-		}
-		os.Exit(0)
+		log.Fatalf("%v", err)
 	}
 	hours, minutes, seconds := time.Clock()
 	fmt.Printf("Текущее время: %d:%d:%d", hours, minutes, seconds)
